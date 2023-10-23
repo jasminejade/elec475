645e9a17adb9c3d56b1693cfc030074b584a7ccc
Vali <18mav2@queensu.ca>
2023-10-22 20:47:29 -0400

    fixed inner loop and added argparser

--- lab2/train.py
+++ lab2/train.py
@@ -1,18 +1,14 @@
 import argparse
-from pathlib import Path
-
 import torch
-import sys
 import tqdm
 
 from torchvision import transforms
-from torch.utils.data import DataLoader, BatchSampler
+from torch.utils.data import DataLoader
 from torch.optim import lr_scheduler
 from matplotlib import pyplot as plt
 
 from custom_dataset import *
 from AdaIN_net import *
-from sampler import *
 
 
 def train_transform():
@@ -24,10 +20,8 @@ def train_transform():
     return transforms_list
 
 
-def train(content_iter, style_iter, adam, schedule, network, epochs, batch):
+def train(content_iter, style_iter, adam, schedule, network, epochs, n):
     network.decoder.train()
-    content_weight = 1.0
-    style_weight = 10.0
     losses_train = []
     losses_c = []
     losses_s = []
@@ -36,12 +30,10 @@ def train(content_iter, style_iter, adam, schedule, network, epochs, batch):
         loss_train = 0
         loss_train_c = 0
         loss_train_s = 0
-        for b in range(batch):
+        for b in range(n):
             content_images = next(iter(content_iter)).to(device=device)
             style_images = next(iter(style_iter)).to(device=device)
             loss_c, loss_s = network(content_images, style_images) # this is forward
-            loss_c = content_weight * loss_c
-            loss_s = style_weight * loss_s
             loss = loss_c + loss_s
 
             adam.zero_grad()
@@ -53,12 +45,12 @@ def train(content_iter, style_iter, adam, schedule, network, epochs, batch):
             loss_train_s += loss_s.item()
 
         schedule.step()
-        losses_train += [loss_train / (len(content_iter.dataset)*10)]
-        losses_c += [loss_train_c / len(content_iter.dataset)]
-        losses_s += [loss_train_s / (len(content_iter.dataset)*10)]
+        losses_train += [loss_train / n]
+        losses_c += [loss_train_c / n]
+        losses_s += [loss_train_s / n]
         print("Overall:", losses_train[-1], ", Content:", losses_c[-1], ", Style:", losses_s[-1])
 
-    torch.save(network.decoder.state_dict(), decoderModel)
+    torch.save(network.decoder.state_dict(), args.decoder_pth)
 
     fig, ax = plt.subplots(1,1)
     ax.set_xlabel('epoch')
@@ -66,67 +58,32 @@ def train(content_iter, style_iter, adam, schedule, network, epochs, batch):
     ax.plot(losses_train, label='content + style')
     ax.plot(losses_c, label='content')
     ax.plot(losses_s, label='style')
-    fig.suptitle(f'loss plot, {len(content_iter.dataset)} dataset')
+    fig.suptitle(f'loss plot, {n} dataset')
     ax.legend()
 
     plt.savefig('loss.AdaIN.png')
     plt.show()
 
-
-
-# params = sys.argv[1:]
-# print (params)
-# content_dir = params[1]
-#
-# style_dir = params[3]
-#
-# gamma= float(params[5])
-#
-# epochs = int(params[7])
-#
-# batch = int(params[9])
-#
-# encoderModel = params[11]
-#
-# decoderModel = params[13]
-#
-# decoder_PNG = params[15]
-#
-# cuda = params[17]
-#
-# save_model_interval=1000
-
-
 content_dir = str("D:/475/elec475/lab2/datasets/COCO1k/")
-
 style_dir = str("D:/475/elec475/lab2/datasets/wikiart1k/")
 
-gamma= float(0.9)
+parser = argparse.ArgumentParser()
+parser.add_argument('--content_dir', type=str, default=content_dir)
+parser.add_argument('--style_dir', type=str, default=style_dir)
+parser.add_argument('--gamma', type=float, default=0.9)
+parser.add_argument('-epochs', type=int, default=20)
+parser.add_argument('-batch', type=int, default=20)
+parser.add_argument('--encoder_pth', type=str, default="encoder.pth") # vgg
+parser.add_argument('--decoder_pth', type=str, default="decoder.pth")
+parser.add_argument('--loss_plot', type=str, default="loss.AdaIN.png")
+args = parser.parse_args()
 
-epochs = int(20)
-
-batch = int(20)
-
-encoderModel = "encoder.pth"
-
-decoderModel = "decoder.pth"
-
-decoder_PNG = "decoder.png"
-
-cuda = 'Y'
-
-save_model_interval=1000
-
-'''
-['-content_dir', './../../../datasets/COCO100/', '-style_dir', './../../../datasets/wikiart100/', '-gamma', '1.0', '-e', '20', '-b', '20', '-l', 'encoder.pth', '-s', 'decoder.pth', '-p', 'decoder.png', '-cuda', 'Y']
-
-'''
 device = torch.device('cuda')
 model = encoder_decoder()
 decoder_model = model.decoder
 encoder_model = model.encoder
 
-encoder_model.load_state_dict(torch.load('encoder.pth'))
+encoder_model.load_state_dict(torch.load(args.encoder_pth))
 encoder_model = nn.Sequential(*list(encoder_model.children())[:31])
 
 network = AdaIN_net(encoder_model, decoder_model)
@@ -136,17 +93,15 @@ network.to(device=device)
 transform_content = train_transform()
 transform_style = train_transform()
 
-content_data = custom_dataset(content_dir, transform_content)
-style_data = custom_dataset(style_dir, transform_style)
+content_data = custom_dataset(args.content_dir, transform_content)
+style_data = custom_dataset(args.style_dir, transform_style)
+n = len(content_data)
 
+content_iter = (DataLoader(content_data, batch_size=args.batch, shuffle=True))
+style_iter = (DataLoader(style_data, batch_size=args.batch, shuffle=True))
 
-# shuffle=True,
-#   sampler=InfiniteSamplerWrapper(content_data)
-content_iter = (DataLoader(content_data, batch_size=batch, shuffle=True))
-style_iter = (DataLoader(style_data, batch_size=batch, shuffle=True))
+adam = torch.optim.Adam(network.decoder.parameters(), lr=1e-4)
+schedule = lr_scheduler.ExponentialLR(adam, gamma=args.gamma)
 
-adam = torch.optim.Adam(network.decoder.parameters(), lr=1e-5, weight_decay=5e-5)
-schedule = lr_scheduler.ExponentialLR(adam, gamma=gamma)
-
-train(content_iter=content_iter, style_iter=style_iter, adam=adam, schedule=schedule, network=network, epochs=epochs, batch=batch)
+train(content_iter=content_iter, style_iter=style_iter, adam=adam, schedule=schedule, network=network, epochs=args.epochs, n=n)
 
