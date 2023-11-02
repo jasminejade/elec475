Updated and fixed forward loss, and classification (softmax) works OK

lab3/network.py
@@ -115,7 +115,7 @@ class Network(nn.Module):
         if self.classifier is not None:
             pass
         else:
-            self.classifer= VanillaModel.classifier
+            self.classifer = VanillaModel.classifier
             # initalize frontend weights
             for param in self.classifier.parameters():
                 nn.init.normal_(param, mean=0.0, std=0.0)
@@ -124,18 +124,19 @@ class Network(nn.Module):
         self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
 
     def encode(self, X):
-        return self.backend(X)
+        return self.encoder(X)
 
-    def calc_loss(self, input, target):
-        assert (input.size() == target.size())
-        assert (target.requires_grad is False)
-        return self.cross_entropy_loss(input, target)
+    def calc_loss(self, _input, _target):
+        #assert (_input.size() == _target.size())
+        assert (_target.requires_grad is False)
+        return self.cross_entropy_loss(_input, _target)
 
     def classify(self, X):
-        out = self.features(X)
+        out = self.encode(X)
         out = self.avgpool(out)
         out = torch.flatten(out, 1)
         out = self.classifier(out)
+        out = F.softmax(out, 1)
         return out
 
     # implementation of classification frontend
@@ -148,8 +149,7 @@ class Network(nn.Module):
     # and outputs a 1xC tensor where C is the number of class labels in the dataset
     def forward(self, X, C=100):
         if self.training:
-            X = self.encode(X)
-            out = self.classify(X[-1])
+            out = self.classify(X)
 
             return out
         else:

lab3/vanilla.py
@@ -18,6 +18,7 @@ from network import *
 def train(network, train_loader, optimizer, schedule, epochs, n=1, device='cuda'):
     # load vgg backend pre-trained parameters that were distributed in lab2
     # or make your own with random weights
+    network.to(device=device)
     network.classifier.train()
     losses_train = []
     for i in tqdm.tqdm(range(epochs)):
@@ -26,9 +27,11 @@ def train(network, train_loader, optimizer, schedule, epochs, n=1, device='cuda'
 
             imgs = imgs.to(device=device)
             labels = labels.to(device=device)
+            #print(imgs, labels)
 
             # calculate forward method for classification here
             output = network(imgs) # forward method
+
             loss = network.calc_loss(output, labels)
 
             optimizer.zero_grad()
@@ -41,27 +44,51 @@ def train(network, train_loader, optimizer, schedule, epochs, n=1, device='cuda'
         losses_train += [loss_train/n]
         print("Overall Loss: ", losses_train[-1])
         
-    torch.save(network.classifier.state_dict(args.classifier_pth))
+    torch.save(network.classifier.state_dict(), args.classifier_pth)
 
     plt.plot(losses_train)
     plt.xlabel('epochs')
     plt.ylabel('losses')
+    plt.title(f'vanilla model, batch size {n}')
 
     plt.savefig(args.loss_plot)
     plt.show()
 
 # implement fucntion later
-def test():
-    return None
+def test(network, test_loader, optimizer, schedule, epochs, n=1, device='cuda'):
+    network.to(device=device)
+    network.classifier.eval()
+
+    total = 0
+    with torch.no_grad():
+        for imgs, labels in test_loader:
+            imgs = imgs.to(device=device)
+            labels = labels.to(device=device)
+
+            output = network(imgs) # forward method
+
+            # for each j array in output, get the index of the largest value
+            # this returns an 1 dim array with the class prediction for each j
+            index = torch.argmax(output, dim=1) # get index of class with highest probability
+            print(len(labels))
+            classified = 0
+            assert (index.size() == labels.size())
+            for i in range(0, len(index)):
+                if index[i] == labels[i]:
+                    classified += 1
+            print(f'classified: {classified}/{len(index)}')
+            total += classified
+    print(f'total classified: {total}/{10000}')
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--gamma', type=float, default=0.9)
 parser.add_argument('--epochs', type=int, default=20)
-parser.add_argument('--batch', type=int, default=40)
+parser.add_argument('--batch', type=int, default=2048)
 parser.add_argument('--encoder_pth', type=str, default="encoder.pth") # vgg
 parser.add_argument('--classifier_pth', type=str, default="classifier.pth")
 parser.add_argument('--loss_plot', type=str, default="loss.Vanilla.png")
-parser.add_argument('--training', type=str, default="y")
+parser.add_argument('--training', type=str, default="n")
 args = parser.parse_args()
 
 device = torch.device('cuda')
@@ -73,7 +100,6 @@ encoder.load_state_dict(torch.load(args.encoder_pth))
 encoder = nn.Sequential(*list(encoder.children())[:31])
 
 network = Network(encoder, classifier)
-network.to(device=device)
 
 train_transform = transforms.Compose([transforms.ToTensor()])
 train_set = CIFAR100('./data/cifar100', train=True, download=True, transform=train_transform)
@@ -86,6 +112,8 @@ adam = torch.optim.Adam(network.classifier.parameters(), lr=1e-4)
 schedule = lr_scheduler.ExponentialLR(adam, gamma=args.gamma)
 
 if args.training == "y":
-    train(network=network, train_loader=train_loader, optimizer=adam, schedule=schedule, epochs=args.epochs)
+    train(network=network, train_loader=train_loader, optimizer=adam, schedule=schedule, epochs=args.epochs, n=args.batch)
 else:
-    test()
+    class_dict = torch.load(args.classifier_pth)
+    network.classifier.load_state_dict(class_dict)
+    test(network=network, test_loader=test_loader, optimizer=adam, schedule=schedule, epochs=args.epochs, n=args.batch)